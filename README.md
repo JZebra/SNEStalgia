c6a512250fbfcf487b5bf29f7d7221728facf0f5
App Academy Student <student@appacademy.io>
2014-09-05 10:40:39 -0700

    added framework for Jasmine

---
+++ README.md
@@ -0,0 +1,10 @@
+##SNEStalgia
+
+####Features:
+
+- Remake of a memory game with my favorite SNES sprites as the cards
+
+####To do:
+
+- When cards are matched, they disappear and reveal part of the background
+- Completed cards are placed in a row so you can see all of them. 
---
+++ test/spec/board.js
@@ -0,0 +1,17 @@
+describe('board', function () {
+  
+  it('displays 36 cards', function(){
+    
+  });
+  it('reveals card faces when they are clicked', function(){
+    
+  });
+  it('flips cards back when they do not match', function(){
+    
+  });
+  it('keeps cards revealed if they match' function(){
+    
+  });
+  
+  
+});
